076aacdc48a6f5f252b1b6f477f818c9d894f121
AKITO-PC\akito <akitosun0614@gmail.com>
2022-03-23 11:27:55 +0800

    add no.1 two sum

---
+++ LeetCode/HashTable/No1.TwoSum/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable.No1.TwoSum
+{
+    /// <summary>
+    /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    /// You can return the answer in any order.
+    /// </summary>
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            //O(n^2)
+            //var result = new int[2];
+            //for (var i = 0; i < nums.Length; i++)
+            //{
+            //    var assert = target - nums[i];
+            //    for (var j = i+1; j < nums.Length; j++)
+            //    {
+
+            //        if (nums[j] == assert)
+            //        {
+            //            result[0] = i;
+            //            result[1] = j;
+            //        }
+            //    }
+            //}
+            //return result;
+
+            //O(n)
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (dict.ContainsKey(diff))
+                    return new int[] { dict[diff],i };
+                else
+                    dict.TryAdd(nums[i], i);
+            }
+            return new int[] { 0, 0 };
+
+        }
+    }
+}
---
+++ LeetCodeTests/HashTable/No1.TwoSum/SolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.HashTable.No1.TwoSum;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LeetCode.HashTable.No1.TwoSum.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        [DataRow(new int[]{ 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [DataRow(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [DataRow(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        public void TwoSumTest(int[] nums,int target,int[] expected)
+        {
+            var sut=new Solution();
+            var actual = sut.TwoSum(nums, target);
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
