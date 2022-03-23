831017ba42a1ce8ad7b4ff02962f2ca099b49e36
AKITO-PC\akito <akitosun0614@gmail.com>
2022-03-23 14:43:23 +0800

    add no 14. Longest Common Prefix

---
+++ LeetCode/String/No14.LongestCommonPrefix/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.String.No14.LongestCommonPrefix
+{
+    /// <summary>
+    ///   Write a function to find the longest common prefix string amongst an array of strings.
+    ///   If there is no common prefix, return an empty string "".
+    /// </summary>
+    public class Solution
+    {
+        /// <summary>
+        /// Horizontal scanning
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string LongestCommonPrefixByHorizontalScanning(string[] input)
+        {
+            if (input.Length == 0) return "";
+            // step 1. let first input array element be common prefix
+            var prefix = input[0];
+            // step 2. start from second input array element to compare
+            for (int i = 1; i < input.Length; i++)
+            {
+                // step 3. cut one char from common prefix string end and re compare again until prefix found in element index 0 or common prefix become empty
+                while (input[i].IndexOf(prefix)!=0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                    if (prefix == string.Empty)
+                    {
+                        return string.Empty;
+                    }
+                }
+            }
+
+            return prefix;
+        }
+        /// <summary>
+        /// Vertical scanning
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string LongestCommonPrefixByVerticalScanning(string[] input)
+        {
+            var prefix = string.Empty;
+            var minimumlength = input.Min(x => x.Length);
+            // step 1. start from 0 to minimumlength
+            for (int i = 0; i < minimumlength; i++)
+            {
+                // step 2. define 1 char from first input array element and traverse
+                var c = input[0].ToCharArray()[i];
+                foreach (var word in input)
+                {
+                    // compare char
+                    if (word.ToCharArray()[i] != c)
+                    {
+                        return prefix;
+                    }
+                }
+
+                prefix += c;
+            }
+
+            return prefix;
+        }
+    }
+}
---
+++ LeetCodeTests/String/No14.LongestCommonPrefix/SolutionTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.String.No14.LongestCommonPrefix;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace LeetCode.String.No14.LongestCommonPrefix.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        [DataRow(new string[]{ "flower", "flow", "flight" },"fl")]
+        [DataRow(new string[] { "dog", "racecar", "car" }, "")]
+        [DataRow(new string[] { "leets", "leetcode", "leet","leeds" }, "lee")]
+        public void HorizontalScanningTest(string[] input,string output)
+        {
+            var sut = new Solution();
+            var actual = sut.LongestCommonPrefixByHorizontalScanning(input);
+            actual.Should().BeEquivalentTo(output);
+        }
+        [TestMethod()]
+        [DataRow(new string[] { "flower", "flow", "flight" }, "fl")]
+        [DataRow(new string[] { "dog", "racecar", "car" }, "")]
+        [DataRow(new string[] { "leets", "leetcode", "leet", "leeds" }, "lee")]
+        public void VerticalScanningTest(string[] input, string output)
+        {
+            var sut = new Solution();
+            var actual = sut.LongestCommonPrefixByVerticalScanning(input);
+            actual.Should().BeEquivalentTo(output);
+        }
+    }
+}
